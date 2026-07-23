55227adbcb308f27a1e244f2567c99ba8acb625e
allex <allexsmith@MacOS-btw.local>
2026-07-23 14:43:21 -0700

    moved setup local to my uchicago directory as it has the latest run of quickfit

--- setup_local.sh
+++ setup_local.sh
@@ -2,6 +2,8 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 source $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "views LCG_108 x86_64-el9-gcc14-opt"
 
-QUICKFIT_DIR=/home/mhance/pyhs3/quickFit
+# Point at your local quickFit build (must be compiled against the same LCG/ROOT
+# sourced above). Override for a different checkout with:  export QUICKFIT_DIR=...
+QUICKFIT_DIR=${QUICKFIT_DIR:-/home/allex/git-projects/quickFit}
 export PATH=${PATH}:${QUICKFIT_DIR}/bin
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${QUICKFIT_DIR}/lib
